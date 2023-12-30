5f168097a182c8f26dfbd482fc24f33a7e8dd4e5
Ezequiel <cruzezequiel81@gmail.com>
2023-12-30 15:47:29 -0500

    Made keys in keyboard when pressed to return data-key assign to button

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    createSquare()
-     
-    function createSquare(){
-        const gameboard = document.getElementById("board")
+document.addEventListener("DOMContentLoaded", () => {
+  createSquare();
 
-        for(let i = 0; i < 30; i++){
-            let square = document.createElement("div")
-            square.classList.add("square")
-            square.setAttribute('id', i + 1)
-            gameboard.appendChild(square)
-        }
+  const key = document.querySelectorAll('.keyboard-row button')
+
+  for(let i = 0; i < key.length; i++){
+    key[i].onclick = ({target}) => {
+        const key = target.getAttribute("data-key")
+
+        console.log(key)
     }
-})
+  }
+
+  function createSquare() {
+    const gameboard = document.getElementById("board");
+
+    for (let i = 0; i < 30; i++) {
+      let square = document.createElement("div");
+      square.classList.add("square");
+      square.setAttribute("id", i + 1);
+      gameboard.appendChild(square);
+    }
+  }
+});
